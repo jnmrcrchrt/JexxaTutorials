12b283c7ff2951984535de1a14017ea5164d1539
di40231 <jan.reichert@dillinger.biz>
2020-09-09 09:09:24 +0200

    Kafka Adapter Grundgerüst

--- jexxa-core/src/main/java/io/jexxa/infrastructure/drivingadapter/kafka/KafkaAdapter.java
+++ jexxa-core/src/main/java/io/jexxa/infrastructure/drivingadapter/kafka/KafkaAdapter.java
@@ -1,9 +1,11 @@
 package io.jexxa.infrastructure.drivingadapter.kafka;
 
 
+import java.util.Arrays;
 import java.util.Properties;
 
 
+import io.jexxa.infrastructure.drivingadapter.messaging.JMSConfiguration;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -15,6 +17,8 @@ public class KafkaAdapter implements  IDrivingAdapter
 
     public static final String BOOSTRAP_SERVER = "localhost:9092";
     public static final String AUTO_COMMIT = "false";
+    public static final String KEY_DESERIALIZER = "org.apache.kafka.common.serialization.StringDeserializer";
+    public static final String VALUE_DESERIALIZER = "org.apache.kafka.common.serialization.StringDeserializer";
 
 
 
@@ -32,6 +36,9 @@ public class KafkaAdapter implements  IDrivingAdapter
     {
 
         KafkaConsumer kafkaConsumer = (KafkaConsumer) (object);
+        KafkaConfiguration kafkaConfiguration  = getConfiguration(object);
+
+
 
     }
 
@@ -46,6 +53,16 @@ public class KafkaAdapter implements  IDrivingAdapter
     public void stop()
     {
 
+    }
+
+
+    private KafkaConfiguration getConfiguration(Object object)
+    {
+        return Arrays.stream(object.getClass().getMethods())
+                .filter(method -> method.isAnnotationPresent(KafkaConfiguration.class))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Given object does not provide a " + KafkaConfiguration.class.getSimpleName()))
+                .getDeclaredAnnotation(KafkaConfiguration.class);
     }
 
 }
